4532d80dd27bd5dc1e74c20133b79df6c9e6330c
Demon <zero2211@protonmail.com>
2021-09-23 18:02:18 -0400

    started creating variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,10 @@ let q1 = document.getElementsByClassName('1')
 let q2 = document.getElementsByClassName('2')
 let q3 = document.getElementsByClassName('3')
 let q4 = document.getElementsByClassName('4')
+let endscreen = document.getElementsByClassName("endscreen")
+let enterinitals = document.getElementsByClassName("enterinitals")
+let submitHS = document.getElementsByClassName("submitHS")
+
+let highscore = document.getElementsByClassName("highscore")
+let hs = document.getElementsByClassName("hs")
+let
